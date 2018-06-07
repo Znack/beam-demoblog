a7cbc85fea041e5caba86d0d92a4ca001f8b524a
znack <scher56@gmail.com>
2018-06-07 17:14:01 +0700

    add creating post

--- src/Queries/Post.hs
+++ src/Queries/Post.hs
@@ -1,7 +1,15 @@
 module Queries.Post where
 
+import Control.Lens
+import Control.Monad
+import Control.Monad.Trans
+import Control.Monad.Trans.Maybe
+import Data.Maybe (listToMaybe)
+import Data.Text (Text)
+
 import Database.Beam
 import Database.Beam.Backend.SQL
+import qualified Database.Beam.Backend.SQL.BeamExtensions as BeamExtensions
 import Database.Beam.Postgres
 import Database.Beam.Postgres.Syntax
 import Database.Beam.Query
@@ -18,5 +26,38 @@ getAll = queryGetAll _post
 getById :: Int -> Pg (Maybe (PostT Identity))
 getById = queryGetByPK _post PostId
 
-createPost :: Post -> Pg (Maybe (PostT Identity))
-createPost = create _post
+createPost :: Text -> Text -> Int -> [Int] -> Pg (Maybe (PostT Identity, [Tag]))
+createPost title content authorId tags =
+  runMaybeT $ do
+    tagEntities <-
+      MaybeT $
+      fmap sequence <$> forM tags $ \tagId ->
+        runSelectReturningOne .
+        select .
+        filter_ (\tag -> pk tag ==. (val_ . TagId . fromIntegral) tagId) . all_ $
+        _tag db
+    createdPost <-
+      MaybeT $
+      listToMaybe <$>
+      BeamExtensions.runInsertReturningList
+        (_post db)
+        (insertExpressions
+           [ Post
+               default_
+               (val_ title)
+               (val_ content)
+               (AuthorId $ fromIntegral authorId)
+           ])
+    let postId :: Int
+        postId = unSerial $ _postId createdPost
+        createTagOfPost tagId =
+          TagOfPost
+            default_
+            (PostId $ fromIntegral postId)
+            (TagId $ fromIntegral tagId)
+    _ <-
+      lift $
+      BeamExtensions.runInsertReturningList
+        (_tagOfPost db)
+        (insertExpressions $ map createTagOfPost tags)
+    pure (createdPost, tagEntities)
