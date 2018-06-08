8a7c716e087fa2398c5ad3b06210a738d26d5245
znack <scher56@gmail.com>
2018-06-08 17:19:17 +0700

    add typings for generic create

--- src/Queries/Generic/Create.hs
+++ src/Queries/Generic/Create.hs
@@ -6,10 +6,32 @@ import Database.Beam
 import Database.Beam.Backend.SQL
 import qualified Database.Beam.Backend.SQL.BeamExtensions as BeamExtensions
 import Database.Beam.Postgres
+import Database.Beam.Postgres.Syntax
+import Database.Beam.Schema.Tables
+
+import GHC.Generics
 
 import RunDB
 import Schema.Database
 
-create table entity =
-  fmap listToMaybe <$> BeamExtensions.runInsertReturningList (table db) $
+runQuery ::
+     ( Table table
+     , FromBackendRow Postgres (table Identity)
+     , FieldsFulfillConstraint (HasSqlValueSyntax PgValueSyntax) table
+     )
+  => TableSelector table
+  -> table Identity
+  -> Pg [table Identity]
+runQuery table entity =
+  BeamExtensions.runInsertReturningList (table db) $
   insertExpressions [val_ entity]
+
+create ::
+     ( Table table
+     , FromBackendRow Postgres (table Identity)
+     , FieldsFulfillConstraint (HasSqlValueSyntax PgValueSyntax) table
+     )
+  => TableSelector table
+  -> table Identity
+  -> Pg (Maybe (table Identity))
+create table entity = listToMaybe <$> runQuery table entity
